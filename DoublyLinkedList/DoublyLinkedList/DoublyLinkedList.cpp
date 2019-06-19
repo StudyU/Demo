3bdb8407c8107f16c84e8a5f18321fbab480b562
StudyU <1550051771@qq.com>
2019-06-19 14:23:34 +0800

    add

--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cpp
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cpp
@@ -2,29 +2,55 @@
 
 bool DoublyLinkedList::clear()
 {
+	if (m_List)
+	{
+		// 向前清空
+		DoublyNode* pPre = m_List->preNode;
+		while (pPre)
+		{
+			SAFE_FREE(pPre);
+			pPre = pPre->preNode;
+		}
+
+		// 向后清空
+		DoublyNode* pNext = m_List->nextNode;
+		while (pNext)
+		{
+			SAFE_FREE(pNext);
+			pNext = pNext->nextNode;
+		}
+
+		SAFE_FREE(m_List);
+	}
+
+	m_Length = 0;
 
 	return true;
 }
 
 bool DoublyLinkedList::empty()
 {
-
-	return true;
+	return m_Length == 0;
 }
 
-bool DoublyLinkedList::erase(Node* pNode)
+bool DoublyLinkedList::erase(DoublyNode* pNode)
 {
+	pNode->preNode->nextNode = pNode->nextNode;
+	pNode->nextNode->preNode = pNode->preNode;
+	SAFE_FREE(pNode);
 
 	return true;
 }
 
-bool DoublyLinkedList::push(Node* pNode)
+bool DoublyLinkedList::push(DoublyNode* pNode)
 {
-
+	pNode->preNode = m_List;
+	m_List->nextNode = pNode;
+	m_List = pNode;
 	return true;
 }
 
-bool DoublyLinkedList::insert(Node* preNode, Node* pNode)
+bool DoublyLinkedList::insert(DoublyNode* preNode, DoublyNode* pNode)
 {
 
 	return true;
--- DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.h
@@ -1,29 +1,40 @@
 #ifndef DOUBLY_LINKED_LIST_H_
 #define DOUBLY_LINKED_LIST_H_
+//https://www.cnblogs.com/skywang12345/p/3561803.html
+#define SAFE_FREE(p) if (p) { delete (p); (p) = nullptr; };
+#define SAFE_FREE_ARRAY(p) if (p) { delete[] (p); (p) = nullptr; };
 
 // Ë«ÏòÁ´±í
+template<typename TYPE>
 struct Node
 {
-	Node():preNode(nullptr), nextNode(nullptr), data(0)
+	Node():preNode(nullptr), nextNode(nullptr)
 	{}
 	Node* preNode;
 	Node* nextNode;
-	int data;
-	int index;
+	TYPE data;
 };
+
+typedef Node<int> DoublyNode;
 
 class DoublyLinkedList
 {
 public:
-	bool init();
+	DoublyLinkedList():m_Length(0)
+	{
+		m_List->preNode = nullptr;
+		m_List->nextNode = nullptr;
+		m_List = nullptr;
+	};
+
 	bool clear();
 	bool empty();
-	bool erase(Node* pNode);
-	bool insert(Node* preNode, Node* pNode);
-	bool push(Node* pNode);
+	bool erase(DoublyNode* pNode);
+	bool insert(DoublyNode* preNode, DoublyNode* pNode);
+	bool push(DoublyNode* pNode);
 	int length();
 private:
-	Node* m_List;
+	DoublyNode* m_List;
 	int m_Length;
 };
 
