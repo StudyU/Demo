534f0c16598e2c96b76fe6098a48a6bdbbd7476f
Yudl <1550051771@qq.com>
2020-06-12 16:25:31 +0800

    dll

--- Dll/Dll/TestMath.cpp
+++ Dll/Dll/TestMath.cpp
@@ -1,11 +1,25 @@
 #include <stdio.h>
-#include "..\Math\Math.h"
-
-//__declspec(dllimport) double Sub(double a, double b);
-
+#include <windows.h>
+#include <string>
+/*
+https://blog.csdn.net/g5dsk/article/details/6680698
+*/
+typedef int(__cdecl* MYPROC)(int,int);
 int main(int argc, char **argv)
 {
-	double result = Add(1, 2);
-	printf("result = %f", result);
+	std::string str = "D:/GitHub/Demo/Dll/x64/Debug/Math.dll";
+	// 显式调用：使用LoadLibrary载入动态链接库-GetProcAddress获取某函数地址
+	HMODULE hid = LoadLibraryExA(str.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+	if (hid == nullptr)
+	{
+		return 0;
+	}
+	MYPROC p = (MYPROC)GetProcAddress(hid, "Add");
+	if (p == nullptr)
+	{
+		return 0;
+	}
+	int result = p(1, 2);
+	printf("result = %d", result);
 	return 0;
 }
--- Dll/Math/Math.h
+++ Dll/Math/Math.h
@@ -1,3 +1,7 @@
 #pragma once
-int Add(int, int);
+__declspec(dllexport) int Add(int, int);
 int Mul(int, int);
+
+/*
+https://www.cnblogs.com/depend-wind/articles/11126333.html
+*/
